bc4ba68b686954f49638d59c1ccc7292d105ac47
Oleksa <djmelyck@gmail.com>
2024-03-06 13:53:48 +0200

    first commit

---
+++ program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+Dictionary<int, string> dic = new Dictionary<int, string>();
+dic.Add(1, "dsadas");
+dic.Add(2, "curk");
+dic.Add(3, "dsadas");
+dic.Add(4, "dsadas");
+dic.Add(5, "falm");
+dic.Add(6, "curk");
+dic.Add(7, "fifa");
+dic.Add(8, "dsadas");
+
+Dictionary<int, string> dic2 = new Dictionary<int, string>();
+
+foreach (KeyValuePair<int, string> pair in dic)
+{
+    if(!dic2.ContainsValue(pair.Value))
+    {
+        dic2.Add(pair.Key, pair.Value);
+    }
+}
+
+string json = JsonSerializer.Serialize(dic2);
+Console.WriteLine(json);
+Console.ReadKey();
